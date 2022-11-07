e5e419c38a5b918897d6cc5ea72ffd7e9489b2b1
vedant713 <66007382+vedant713@users.noreply.github.com>
2022-11-07 18:54:35 +0530

    Add files via upload

---
+++ EXP6/pca.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+dataset = pd.read_csv(url, names=names)
+print("First 5 values of the Dataset:\n",dataset.head())
+
+X = dataset.drop('Class', axis=1)
+y = dataset['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+pca = PCA()
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+pca = PCA(n_components=1)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+classifier = RandomForestClassifier(max_depth=2, random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:\n", cm)
+print('\nAccuracy: ', accuracy_score(y_test, y_pred))
