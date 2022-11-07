c7bae552750246d1bf6fe2fcb53fca9d88e5d487
vedant713 <66007382+vedant713@users.noreply.github.com>
2022-11-07 18:31:16 +0530

    Add files via upload

---
+++ EXP2/logistic.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+
+dataset = pd.read_csv("User_Data.csv")
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+sc_x = StandardScaler()
+X_train = sc_x.fit_transform(X_train)
+X_test = sc_x.transform(X_test)
+print (X_train[0:10, :])
+
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print ("Confusion Matrix : \n", cm)
+print ("Accuracy : ", accuracy_score(y_test, y_pred))
+
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+   				      stop = X_set[:, 0].max() + 1, step = 0.01),
+   				      np.arange(start = X_set[:, 1].min() - 1,
+   				      stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(
+   		 np.array([X1.ravel(), X2.ravel()]).T).reshape(
+   		 X1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+	
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+   	c = ListedColormap(('red', 'green'))(i), label = j)    
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
